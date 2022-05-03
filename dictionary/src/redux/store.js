ecce9b08325a38a59fab2c1cf35cf700d30106be
Zaiko Euheni <rahunak@yandex.by>
2022-05-03 13:39:33 +0300

    Updates

--- dictionary/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import dictionaryReducer from './dictionaryReducer'
-
-export const store = configureStore({
-  reducer: {
-    dictionary: dictionaryReducer
-  }
-})
-window.store=store;
