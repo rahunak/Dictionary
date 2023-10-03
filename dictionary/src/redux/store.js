ed1a26199af0a873f7a9d8cf52e1d562d26e7f85
rahunak <euhene@linnovate.net>
2023-10-03 12:23:13 +0300

    fix: фикс конфликта3 + деплой по жести

--- dictionary/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import dictionaryReducer from './dictionaryReducer';
-
-const store = configureStore({
-  reducer: {
-    dictionary: dictionaryReducer,
-  },
-});
-export default store;
